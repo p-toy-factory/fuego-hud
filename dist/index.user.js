be2ac99e87195fb20ab487a89c82bb7a83d1c276
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2024-05-27 00:37:06 +0800

    chore: release v1.0.1

--- dist/index.user.js
+++ dist/index.user.js
@@ -12,7 +12,7 @@
 // @supportURL   https://github.com/p-toy-factory/fuego-hud/issues
 // @require      https://cdn.jsdelivr.net/npm/rxjs@7.8.0/dist/bundles/rxjs.umd.min.js
 // @run-at       document-body
-// @version      1.0.0
+// @version      1.0.1
 // ==/UserScript==
 (() => {
   "use strict";
@@ -33,7 +33,6 @@
       this.cleanupsManager.add(e);
     }
     connectedCallback() {
-      if (!this.props) throw new TypeError(`${this.nodeName}'s props is required`);
       const e = this.attachShadow({
         mode: "open"
       }), n = this.connectedCallbackWithProps(this.props);
@@ -43,100 +42,105 @@
       this.cleanupsManager.cleanup();
     }
   }
-  function t(e, n) {
-    return customElements.define(e, n), function(n) {
-      const t = document.createElement(e);
-      return t.props = n, t;
+  function t(e, t) {
+    const s = `fuego-${e}`;
+    // @ts-expect-error Can not be inferred
+    return customElements.define(s, class extends n {
+      connectedCallbackWithProps(e) {
+        return t({
+          props: e,
+          onCleanup: this.onCleanup.bind(this)
+        });
+      }
+    }), function(e) {
+      const n = document.createElement(s);
+      return n.props = e, n;
     };
   }
-  const s = t("fuego-graph", class extends n {
-    connectedCallbackWithProps({data$: e, height: n, width: t}) {
-      const s = document.createElement("canvas");
-      s.height = n, s.width = t;
-      const i = s.getContext("2d"), c = e.subscribe((e => {
-        i.clearRect(0, 0, t, n), i.beginPath(), i.moveTo(0, n - e[0]);
-        for (let t = 1, s = e.length; t < s; t++) {
-          const s = t, c = Math.max(0, n - e[t]);
-          i.lineTo(s, c);
-        }
-        i.stroke();
-      }));
-      return this.onCleanup((() => c.unsubscribe())), s;
-    }
-  }), i = rxjs, c = new i.Observable((e => {
+  const s = t("reactive-element", (({props: {data$: e, height: n, width: t}, onCleanup: s}) => {
+    const c = document.createElement("canvas");
+    c.height = n, c.width = t;
+    const i = c.getContext("2d"), a = e.subscribe((e => {
+      i.clearRect(0, 0, t, n), i.beginPath(), i.moveTo(0, n - e[0]);
+      for (let t = 1, s = e.length; t < s; t++) {
+        const s = t, c = Math.max(0, n - e[t]);
+        i.lineTo(s, c);
+      }
+      i.stroke();
+    }));
+    return s((() => a.unsubscribe())), c;
+  })), c = rxjs, i = new c.Observable((e => {
     let n = 0, t = 0;
-    const s = (0, i.animationFrames)().subscribe((({timestamp: s}) => {
-      const i = Math.floor(s / 1e3);
-      for (let s = 0; s < i - t; s++) e.next(n), n = 0, t = i;
+    const s = (0, c.animationFrames)().subscribe((({timestamp: s}) => {
+      const c = Math.floor(s / 1e3);
+      for (let s = 0; s < c - t; s++) e.next(n), n = 0, t = c;
       n++;
     }));
     return () => s.unsubscribe();
   }));
-  let o, a = null, r = [];
+  let a, o = null, r = [];
   function u() {
-    const e = performance.now(), n = e - o;
-    o = e;
+    const e = performance.now(), n = e - a;
+    a = e;
     for (let e = 0, t = r.length; e < t; e++) r[e](n);
-    a = requestAnimationFrame(u);
-  }
-  function p() {
-    o = performance.now(), a = requestAnimationFrame(u);
+    o = requestAnimationFrame(u);
   }
   function l() {
+    a = performance.now(), o = requestAnimationFrame(u);
+  }
+  function p() {
     return 0 === r.length;
   }
   function d(e) {
     if ("function" != typeof e) throw TypeError("Expected the argument passed to subscribeFrametime to be a function");
-    return l() && (a = requestAnimationFrame(p)), function(e) {
+    return p() && (o = requestAnimationFrame(l)), function(e) {
       r.push(e);
     }(e), function() {
       !function(e) {
         const n = r.indexOf(e);
         r.splice(n, 1);
-      }(e), l() && (cancelAnimationFrame(a), a = null);
+      }(e), p() && (cancelAnimationFrame(o), o = null);
     };
   }
-  const h = new i.Observable((e => d((n => e.next(n)))));
-  function f(e, n) {
-    return t => t.pipe((0, i.scan)(((n, t) => (n.length >= e && n.shift(), n.push(t), 
+  const m = new c.Observable((e => d((n => e.next(n)))));
+  function h(e, n) {
+    return t => t.pipe((0, c.scan)(((n, t) => (n.length >= e && n.shift(), n.push(t), 
     n)), n?.slice() ?? []));
   }
-  const m = t("reactive-element", class extends n {
-    connectedCallbackWithProps({el: e, subscription: n}) {
-      return this.onCleanup((() => n.unsubscribe())), e;
-    }
-  });
-  function b(e, n, t) {
-    const s = document.createElement(e), i = n.subscribe((e => t(s, e)));
-    return m({
-      el: s,
-      subscription: i
-    });
-  }
-  function g(e, n) {
+  const f = t("fps-text", (({onCleanup: e}) => {
+    const n = document.createElement("div"), t = i.pipe((0, c.distinctUntilChanged)()).subscribe((e => {
+      n.textContent = `Framerate: ${e}fps`;
+    }));
+    return e((() => t.unsubscribe())), n;
+  }));
+  function b(e, n) {
     return e.append(...n), e;
   }
+  const g = m.pipe((0, c.bufferTime)(1e3)), x = t("avg-frametime", (({onCleanup: e}) => {
+    const n = document.createElement("span"), t = g.pipe((0, c.map)((e => (e.reduce(((e, n) => e + n), 0) / (e.length || 1)).toFixed(1))), (0, 
+    c.distinctUntilChanged)()).subscribe((e => {
+      n.innerHTML = `Frametime: <sub>avg</sub>${e}`;
+    }));
+    return e((() => t.unsubscribe())), n;
+  })), C = t("max-frametime", (({onCleanup: e}) => {
+    const n = document.createElement("span"), t = g.pipe((0, c.map)((e => e.reduce(((e, n) => Math.max(e, n)), 0).toFixed(1))), (0, 
+    c.distinctUntilChanged)()).subscribe((e => {
+      n.innerHTML = `<sub>max</sub>${e} ms`;
+    }));
+    return e((() => t.unsubscribe())), n;
+  }));
   !function() {
     const e = document.createElement("div");
     e.id = "fuego-hud", e.setAttribute("style", [ "position: fixed", "top: 0", "left: 0", "background: #ffffff7f", "pointer-events: none", "z-index: 114514" ].join("; "));
-    const n = 180, t = new Array(n).fill(0), o = c.pipe(f(n, t)), a = s({
-      data$: o,
+    const n = 180, t = new Array(n).fill(0), c = i.pipe(h(n, t)), a = s({
+      data$: c,
       height: 60,
       width: n
-    }), r = h.pipe(f(n, t)), u = s({
-      data$: r,
+    }), o = m.pipe(h(n, t)), r = s({
+      data$: o,
       height: 50,
       width: n
-    }), p = b("div", c.pipe((0, i.distinctUntilChanged)()), ((e, n) => {
-      e.textContent = `Framerate: ${n}fps`;
-    })), l = function() {
-      const e = h.pipe((0, i.bufferTime)(1e3)), n = e.pipe((0, i.map)((e => (e.reduce(((e, n) => e + n), 0) / (e.length || 1)).toFixed(1))), (0, 
-      i.distinctUntilChanged)()), t = e.pipe((0, i.map)((e => e.reduce(((e, n) => Math.max(e, n)), 0).toFixed(1))), (0, 
-      i.distinctUntilChanged)());
-      return b("div", (0, i.zip)(n, t), ((e, [n, t]) => {
-        e.innerHTML = `Frametime: <sub>avg</sub>${n} / <sub>max</sub>${t} ms`;
-      }));
-    }();
-    g(document.body, [ g(e, [ p, a, l, u ]) ]);
+    }), u = f(), l = b(document.createElement("div"), [ x(), document.createTextNode(" / "), C() ]);
+    b(document.body, [ b(e, [ u, a, l, r ]) ]);
   }();
 })();
